6c1f3c4824ee32087b3fb12062386cf0b678afab
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-03-13 19:56:47 +0100

    feat: added Loader

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "./Header";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
