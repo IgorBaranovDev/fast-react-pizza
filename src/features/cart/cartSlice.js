9cf98781d1cc3dcb851ba1ff07dc1123f3f9221c
ibaranau1 <ibaranau1@exadel.com>
2024-03-18 12:25:00 +0100

    feat: implemented cardSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  //   cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'jgjgj',
+      quantity: 2,
+      unitPrice: 32,
+      totalPrice: 32,
+    },
+  ],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantit(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantit,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
