a2e72f427c2e041b255bab53ad61b83ab4ed7f36
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-03-19 16:07:20 +0100

    feat: created updateItemQuantity

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { decreaseItemQuantit, increaseItemQuantity } from './cartSlice';
+import Button from '../../ui/Button';
+
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantit(pizzaId))}
+      >
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
